ad46d5f08fda4e8b0dcc341487b42d13bda8439d
Peter Lodri <silentpocok@gmail.com>
2016-06-01 19:03:39 +0200

    fix issues 1, modified post content

--- project/starter_app/views.py
+++ project/starter_app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     # messages = Message.objects.order_by('order')
-    messages = ['a', 'b', 'c']
+    messages = ['hello', 'world', "I'm here"]
     context_dict = {
         'messages': messages
     }
